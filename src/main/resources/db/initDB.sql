d68be6ecbb9b77950b39fb7ace049c847ff0807e
Ar-Rafaella-Redis <anvi.grig@gmail.com>
2021-03-01 15:17:32 +0300

    HW3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,8 @@
-DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS global_seq;
+DROP TABLE IF EXISTS user_roles CASCADE ;
+DROP TABLE IF EXISTS users CASCADE ;
+DROP SEQUENCE IF EXISTS global_seq CASCADE;
+
+DROP TABLE IF EXISTS meals CASCADE ;
 
 CREATE SEQUENCE global_seq START WITH 100000;
 
@@ -21,5 +23,15 @@ CREATE TABLE user_roles
     user_id INTEGER NOT NULL,
     role    VARCHAR,
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE meals
+(   id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    user_id     INTEGER            NOT NULL,
+    description VARCHAR            NOT NULL,
+    date_time   TIMESTAMP          NOT NULL,
+    calories    INTEGER DEFAULT 0  NOT NULL,
+    CONSTRAINT meal_idx UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
